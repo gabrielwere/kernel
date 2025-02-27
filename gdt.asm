f5f7621faf7f02d012a45e58134fbd613d777e0e
gabrielwere <weregabriel91@gmail.com>
2025-02-27 10:45:56 +0300

    switch to protected.this gives us privilege levels

---
+++ gdt.asm
@@ -0,0 +1,30 @@
+gdt_start:
+dd 0x0
+dd 0x0
+
+kernel_code_seg:
+dw 0xffff
+dw 0x0
+db 0x0
+db 10011011b
+db 11001111b
+db 0x0
+
+kernel_data_seg:
+dw 0xffff
+dw 0x0
+db 0x0
+db 10010011b
+db 11001111b
+db 0x0
+
+
+gdt_end:
+
+
+gdt_descriptor:
+dw gdt_end - gdt_start
+dd gdt_start
+
+KERNEL_CODE_SEGMENT equ kernel_code_seg - gdt_start
+KERNEL_DATA_SEGMENT equ kernel_data_seg - gdt_start
--- loader.asm
+++ loader.asm
@@ -3,7 +3,9 @@ FLAGS equ 0x0
 CHECKSUM equ -MAGIC_NUMBER
 KERNEL_STACK_SIZE equ 4096
 
-extern main
+%include "gdt.asm"
+
+[extern main]
 
 section .bss
 	align 4
@@ -20,8 +22,33 @@ section .text
 	
 	loader:
 		mov eax,0xcafebabe
+		
+		;switch to protected mode
+		cli
+
+		lgdt [gdt_descriptor]
+
+		mov eax,cr0
+		or eax,0x1
+		mov cr0,eax
+
+		;flush cs
+		jmp KERNEL_CODE_SEGMENT:PModeMain
+
+
+	PModeMain:
+
+		mov ax,KERNEL_DATA_SEGMENT
+		mov ds,ax
+		mov ss,ax
+		mov es,ax
+		mov fs,ax
+		mov gs,ax
+		
 		mov esp,kernel_stack + KERNEL_STACK_SIZE
 		call main
 
 	loop:
 		jmp loop
+
+
